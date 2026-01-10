39cdb66755fc0543d16e4bf40ee950c968823155
dhyakal-aayush <dhakalaayush35@gmail.com>
2026-01-10 20:15:24 +0545

    Delete all code from repo

--- app/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from app.routers import auth, admin
-
-app = FastAPI(title="HRMS Backend Auth Example")
-
-app.include_router(auth.router)
-app.include_router(admin.router)
--- app/routers/admin.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.core.roles import Roles, role_required
-from fastapi import Depends
-
-router = APIRouter(prefix="/admin", tags=["admin"])
-
-@router.get("/dashboard")
-def admin_dashboard(user=Depends(role_required(Roles.ADMIN))):
-    return {"message": f"Welcome Admin {user['user_id']}"}
