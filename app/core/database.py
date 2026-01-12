237a77c660d90372ae0fc476b31f92d886d9a269
dhakal-aayush <dhakalaayush35@gmail.com>
2026-01-12 11:37:31 +0545

    all

--- app/core/database.py
+++ app/core/database.py
@@ -1,14 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core.config import settings
 
-DATABASE_URL = settings.DATABASE_URL.replace(
-    "postgresql://", "postgresql+psycopg2://"
+DATABASE_URL = "postgresql://hrms_user:Aayush@123@localhost:5432/hrms_db"
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,  
 )
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
