622fea1efda4f2880ab09b6fec58a22b3e8f8de5
dhakal-aayush <dhakalaayush35@gmail.com>
2026-02-02 16:15:40 +0545

    dev

--- app/module/auth/schemas.py
+++ app/module/auth/schemas.py
@@ -5,8 +5,17 @@ from pydantic import BaseModel, EmailStr
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
-    role_name: str  
-
+    name: str        # From "Full Name" field in UI
+    phone: str
+    confirm_password: str
+    citizenship: str
+    department_id: int
+    designation_id: int
+    role_name: str
+    role_id: int
+class Config:
+        from_attributes = True
+    
 # ---------- LOGIN ----------
 class LoginRequest(BaseModel):
     email: EmailStr
